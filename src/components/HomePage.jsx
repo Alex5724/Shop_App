76b40803832d684a2a8c2ef54219a928c8504893
Alex <221934805+Alex5724@users.noreply.github.com>
2026-08-20 20:08:36 +0300

    Modified the home page.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <main className={style.main}>
       <h2 className={style.title}>Welcome, enjoy your shopping!</h2>
-      <Link to="shop">
+      <Link to="shop" className={style.btnContainer}>
         <button className={style.shopBtn}>Shop now</button>
       </Link>
     </main>
